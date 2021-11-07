a96aa637512902678bfc45cdc8d2c34af171df0c
TeaPot98 <trifautan@gmail.com>
2021-11-07 10:49:26 +0200

    Starting using hooks

--- part1/src/App.js
+++ part1/src/App.js
@@ -64,6 +64,47 @@ const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const Click = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text="left"/>
+      <Button onClick={handleRightClick} text="right"/>
+      {right}
+      <History allClicks={allClicks}/>
+    </div>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
@@ -88,6 +129,7 @@ const App = () => {
       <Content content={parts}/>
       <Total content={parts}/>
       <Counter/>
+      <Click/>
     </div>
   )
 }
